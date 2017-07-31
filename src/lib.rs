9ca27dc020fcaced3d8b10a3640f02d89fcf9c25
Pyfisch <pyfisch@gmail.com>
2017-07-31 21:11:49 +0200

    composition events

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ mod modifiers;
 /// Describes the state the key is in.
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub enum State {
+pub enum KeyState {
     /// Key is pressed.
     ///
     /// In JS: "keydown" event firing.
@@ -37,10 +37,39 @@ pub enum State {
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct KeyEvent {
-    pub state: State,
+    pub state: KeyState,
     pub key: Key,
     pub code: Code,
     pub location: Location,
     pub modifiers: Modifiers,
     pub repeat: bool,
+    /// Events with this flag should be ignored in a text editor
+    /// and instead composition events should be used.
+    pub is_composing: bool,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum CompositionState {
+    /// In JS: "compositionstart" event.
+    Start,
+    /// In JS: "compositionupdate" event.
+    Update,
+    /// In JS: "compositionend" event.
+    ///
+    /// In a text editor in this state the data
+    /// should be added to the input.
+    End,
+}
+
+/// Event to expose input methods to program logic.
+///
+/// Provides information about entered sequences from
+/// dead key combinations and IMEs.
+#[derive(Clone, Debug, Eq, PartialEq)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub struct CompositionEvent {
+    pub state: CompositionState,
+    /// Current composition data. May be empty.
+    pub data: String,
 }
