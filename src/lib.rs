210d1a843a3312be8959fd420f51d2d2032f9a03
Mads Marquart <mads@marquart.dk>
2025-06-05 17:22:14 +0200

    Add `impl From<NamedKey> for Key`

--- src/lib.rs
+++ src/lib.rs
@@ -198,6 +198,12 @@ impl FromStr for Key {
     }
 }
 
+impl From<NamedKey> for Key {
+    fn from(value: NamedKey) -> Self {
+        Self::Named(value)
+    }
+}
+
 impl Key {
     /// Determine a *charCode* value for a key with a character value.
     ///
@@ -318,5 +324,10 @@ mod test {
         assert!(is_key_string("A"));
         assert!(!is_key_string("AA"));
         assert!(!is_key_string("	"));
+    }
+
+    #[test]
+    fn into() {
+        assert_eq!(Key::Named(NamedKey::Enter), NamedKey::Enter.into());
     }
 }
